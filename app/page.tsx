0d0ecc328ae372c13378ac2813900987030c7c47
Ajeet <ajeets.shah.87@gmail.com>
2025-01-09 00:45:25 +0530

    git init

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+"use client";
+import { Button } from "rsuite";
+import { DatePicker } from "rsuite";
+
+export default function Page() {
+  return (
+    <div>
+      <Button>Hello World</Button>
+      <DatePicker className="custom-rs-datepicker" />
+    </div>
+  );
+}
