d83991fe7ec071343f93a4d7cd236d6c27a5a03d
Ajeet <ajeets.shah.87@gmail.com>
2025-01-15 13:17:32 +0530

    Refactor

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,8 @@ import { DatePicker } from "rsuite";
 export default function Page() {
   return (
     <div>
-      <Button>Hello World</Button>
+      <h1 className="text-3xl font-bold underline">Hello TailwindCSS!</h1>
+      <Button>I am RSuitejs Button</Button>
       <DatePicker className="custom-rs-datepicker" />
     </div>
   );
